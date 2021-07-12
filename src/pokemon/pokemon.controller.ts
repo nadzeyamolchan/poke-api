e2081a9d3728ac939088e21a91afd2e0cca7cb4f
nadzeya molchan <nadzeya.molchan@itechart-group.com>
2021-07-12 16:32:05 +0300

    refactor pokemon controller

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -8,19 +8,34 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Pokemon } from './pokemon.entity';
 import { Repository } from 'typeorm';
+import { Type } from './type.entity';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(
     @InjectRepository(Pokemon)
     private readonly pokemonRepository: Repository<Pokemon>,
+    @InjectRepository(Type)
+    private readonly pokemonTypeRepository: Repository<Type>,
   ) {}
+
+  @Get()
+  async findAllPokemon() {
+    return await this.pokemonRepository.createQueryBuilder('pokemon').getMany();
+  }
+
+  @Get('/type')
+  async findAllPokemonTypes() {
+    return await this.pokemonTypeRepository
+      .createQueryBuilder('pokemonType')
+      .getMany();
+  }
 
   @Get('/:id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     const pokemon = await this.pokemonRepository.findOne(id);
     if (!pokemon) {
-      throw new NotFoundException();
+      throw new NotFoundException(null, "Pokemon doesn't exist");
     }
     return pokemon;
   }
