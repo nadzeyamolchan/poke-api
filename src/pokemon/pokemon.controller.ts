9d90b4532102c606cb1c8c0140629f49e7b1b971
nadzeya molchan <nadzeya.molchan@itechart-group.com>
2021-07-14 14:49:14 +0300

    refactor pokemon controller

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,31 +1,14 @@
-import {
-  Controller,
-  Get,
-  NotFoundException,
-  Param,
-  ParseIntPipe,
-  Query,
-} from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { Pokemon } from './pokemon.entity';
-import { Repository, SelectQueryBuilder } from 'typeorm';
-import { Type } from './type.entity';
+import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
 export class PokemonController {
-  constructor(
-    @InjectRepository(Pokemon)
-    private readonly pokemonRepository: Repository<Pokemon>,
-    @InjectRepository(Type)
-    private readonly pokemonTypeRepository: Repository<Type>,
-  ) {}
+  constructor(private readonly pokemonService: PokemonService) {}
 
   @Get()
   async findAllPokemon() {
-    return await this.pokemonRepository
-      .createQueryBuilder('pokemon')
-      .leftJoinAndSelect('pokemon.types', 'types')
-      .getMany();
+    return this.pokemonService.getAllPokemon();
   }
 
   @Get('/search')
@@ -33,52 +16,16 @@ export class PokemonController {
     @Query('name') name: string,
     @Query('types') types: string[],
   ): Promise<Pokemon[]> {
-    const query = this.pokemonRepository
-      .createQueryBuilder('pokemon')
-      .leftJoinAndSelect('pokemon.types', 'pokemonTypes');
-
-    if (name) {
-      this.addSearchCriteria(query, name);
-    }
-
-    if (types && types.length > 0) {
-      this.addTypeCriteria(query, types);
-    }
-
-    return await query.orderBy('pokemon.id').getMany();
+    return this.pokemonService.filterPokemonByCriteria(name, types);
   }
 
   @Get('/type')
   async findAllPokemonTypes() {
-    return await this.pokemonTypeRepository
-      .createQueryBuilder('pokemonType')
-      .getMany();
+    return this.pokemonService.getAllPokemonTypes();
   }
 
   @Get('/:id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
-    const pokemon = await this.pokemonRepository.findOne(id);
-    if (!pokemon) {
-      throw new NotFoundException(null, "Pokemon doesn't exist");
-    }
-    return pokemon;
-  }
-
-  private addSearchCriteria(
-    query: SelectQueryBuilder<any>,
-    searchText: string,
-  ): void {
-    query.where(`pokemon.name ILIKE :search`, {
-      search: `%${searchText}%`,
-    });
-  }
-
-  private addTypeCriteria(
-    query: SelectQueryBuilder<any>,
-    types: string[],
-  ): void {
-    query.andWhere(`pokemonTypes.name IN (:types)`, {
-      types: types,
-    });
+    return this.pokemonService.petPokemonById(id);
   }
 }
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pokemon } from './pokemon.entity';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { Type } from './type.entity';
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  constructor(
+    @InjectRepository(Pokemon)
+    private readonly pokemonRepository: Repository<Pokemon>,
+    @InjectRepository(Type)
+    private readonly pokemonTypeRepository: Repository<Type>,
+  ) {}
+  private static addSearchCriteria(
+    query: SelectQueryBuilder<any>,
+    searchText: string,
+  ): void {
+    query.where(`pokemon.name ILIKE :search`, {
+      search: `%${searchText}%`,
+    });
+  }
+
+  private static addTypeCriteria(
+    query: SelectQueryBuilder<any>,
+    types: string[],
+  ): void {
+    query.andWhere(`pokemonTypes.name IN (:types)`, {
+      types: types,
+    });
+  }
+
+  public async getAllPokemon() {
+    return await this.pokemonRepository
+      .createQueryBuilder('pokemon')
+      .leftJoinAndSelect('pokemon.types', 'types')
+      .getMany();
+  }
+
+  public async getAllPokemonTypes() {
+    return await this.pokemonTypeRepository
+      .createQueryBuilder('pokemonType')
+      .getMany();
+  }
+
+  public async petPokemonById(id: number) {
+    const pokemon = await this.pokemonRepository.findOne(id);
+    if (!pokemon) {
+      throw new NotFoundException(null, "Pokemon doesn't exist");
+    }
+    return pokemon;
+  }
+
+  public async filterPokemonByCriteria(name: string, types: string[]) {
+    const query = this.pokemonRepository
+      .createQueryBuilder('pokemon')
+      .leftJoinAndSelect('pokemon.types', 'pokemonTypes');
+
+    if (name) {
+      PokemonService.addSearchCriteria(query, name);
+    }
+
+    if (types && types.length > 0) {
+      PokemonService.addTypeCriteria(query, types);
+    }
+    return await query.orderBy('pokemon.id').getMany();
+  }
+}
